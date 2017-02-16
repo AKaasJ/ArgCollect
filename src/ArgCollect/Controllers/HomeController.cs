38b078a1e3ddfdbb289047bc3d40f2bebacde6ec
AKaasJ <Andreasjoh91@gmail.com>
2017-02-16 12:37:18 +0100

    Add project files.

---
+++ src/ArgCollect/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArgCollect.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to argcollect, the goal of this page is to ... The webpage will present you with a premise which you can agree or disagree with... do you want to participate?";
+            ViewData["Statement"] = "Lorem ipsum > bla bla";
+            ViewData["Argument"] = "Argument #?";
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string primerAreaText)
+        {
+            ViewData["Message"] = "Welcome to argcollect, the goal of this page is to ... The webpage will present you with a premise which you can agree or disagree with... do you want to participate?";
+            ViewData["Statement"] = "Lorem ipsum > bla bla";
+            ViewData["Argument"] = primerAreaText;
+
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ArgCollect/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Write your Javascript code.
+
+$(function(){
+    $("#argumentDiv").hide();
+    $("#buttonsDiv").show();
+});
+
+$("#agreeButton").click(function () {
+    $("#argumentDiv").show();
+    $("#primerArea").val("Prime for agree");
+    $("#buttonsDiv").hide();
+});
+
+$("#disagreeButton").click(function () {
+    $("#argumentDiv").show();
+    $("#primerArea").val("Prime for disagree");
+    $("#buttonsDiv").hide();
+});
